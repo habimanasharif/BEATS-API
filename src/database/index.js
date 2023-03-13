f9be5954680ce61cc0cd36bcee179a130a5ce6f9
sharif Habiman <hdidiersharif@gmail.com>
2023-03-13 19:14:49 +0200

    preparing deploying to vercer

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
 /* eslint-disable no-console */
 import mongoose from 'mongoose';
-import config from '../config';
+// import config from '../config';
 
 export default async () => {
-  await mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.xd7hg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Database Connected');
   })
     .catch((err) => {
